4f9c553b05aa0d3fccc259dd328bda6944a91727
Jethro Estangki <estangkij@gmail.com>
2020-04-21 11:59:22 +0800

    Implement play/pause function

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ const PLAY_PAUSE = document.querySelector(".pause-play");
 
 PLAY_PAUSE.addEventListener("click", () => {
   PLAY_PAUSE.classList.toggle("playing");
+  if (!PLAY_PAUSE.classList[1]) {
+    clearTimeout(timer);
+    return;
+  }
+  startTimer();
 });
-
+let timer;
 let mins = 0;
 let secs = 10;
 let count = 0;
@@ -13,9 +18,9 @@ const FULL_DASH_ARRAY = 283;
 
 const MINUTES = document.querySelector("#minutes");
 const SECS = document.querySelector("#seconds");
-const PLAY = document.querySelector(".play");
+const STATUS = document.querySelector("#status");
 
-const TIME_LIMIT = mins * 60 + secs;
+let TIME_LIMIT = mins * 60 + secs;
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 
@@ -27,7 +32,9 @@ function calculateTimeFraction() {
   const rawTimeFraction = timeLeft / TIME_LIMIT;
   return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
 }
+
 const PATH_REMAINING = document.querySelector("#path-remaining");
+
 function updateCircle() {
   const pathRemaining = `${
     calculateTimeFraction() * FULL_DASH_ARRAY.toFixed(0)
@@ -37,19 +44,20 @@ function updateCircle() {
 }
 
 function startTimer() {
-  setTimeout(() => {
+  timer = setTimeout(() => {
     timePassed++;
     timeLeft = TIME_LIMIT - timePassed;
     if (secs == 0) {
       if (mins == 0) {
         count++;
+        STATUS.textContent = "Break Time!";
         if (count % 4 == 0) {
-          breakTimer(longBreak);
-          return;
+          mins = longBreak;
         } else {
-          breakTimer(shortBreak);
-          return;
+          mins = shortBreak;
         }
+        TIME_LIMIT = mins * 60;
+        timePassed = 0;
       }
       mins--;
       MINUTES.textContent = pad(mins);
@@ -62,14 +70,7 @@ function startTimer() {
   }, 1000);
 }
 
-function breakTimer(minutes) {
-  mins = minutes;
-  startTimer();
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   MINUTES.textContent = pad(mins);
   SECS.textContent = pad(secs);
 });
-
-PLAY.addEventListener("click", startTimer);
