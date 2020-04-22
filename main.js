c7c7a31c99b56df891fdab4832cb2bf6ce67879d
Jethro Estangki <estangkij@gmail.com>
2020-04-22 13:02:34 +0800

    Finish pomodoro with basic functions

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const PLAY_PAUSE = document.querySelector(".pause-play");
+const STOP = document.querySelector(".stop");
 
 PLAY_PAUSE.addEventListener("click", () => {
   PLAY_PAUSE.classList.toggle("playing");
@@ -8,11 +9,21 @@ PLAY_PAUSE.addEventListener("click", () => {
   }
   startTimer();
 });
+
+STOP.addEventListener("click", () => {
+  clearTimeout(timer);
+  mins = pomodoro;
+  secs = 0;
+  count = 0;
+  init();
+});
+
 let timer;
+let pomodoro = 25; // Minutes in a pomodoro
 let mins = 25;
 let secs = 00;
 let count = 0;
-let shortBreakTime = 5; // Minutes
+let shortBreakTime = 1; // Minutes
 let longBreakTime = 15; // Minutes
 let longBreak = 4; // Pomodoros before long break
 const FULL_DASH_ARRAY = 283;
@@ -44,18 +55,29 @@ function updateCircle() {
   PATH_REMAINING.setAttribute("stroke-dasharray", pathRemaining);
 }
 
+let breakTime = false;
+
 function startTimer() {
   timer = setTimeout(() => {
     timePassed++;
     timeLeft = TIME_LIMIT - timePassed;
     if (secs == 0) {
       if (mins == 0) {
-        count++;
-        STATUS.textContent = "Break Time!";
-        if (count % longBreak == 0) {
-          mins = longBreakTime;
+        if (!breakTime) {
+          count++;
+          breakTime = true;
+          STATUS.textContent = "Break Time!";
         } else {
-          mins = shortBreakTime;
+          breakTime = false;
+          mins = pomodoro;
+          STATUS.textContent = "Pomodoro";
+        }
+        if (breakTime) {
+          if (count % longBreak == 0) {
+            mins = longBreakTime;
+          } else {
+            mins = shortBreakTime;
+          }
         }
         TIME_LIMIT = mins * 60;
         timePassed = 0;
@@ -64,17 +86,13 @@ function startTimer() {
       MINUTES.textContent = pad(mins);
       secs = 60;
     }
+
     secs--;
     SECS.textContent = pad(secs);
     updateCircle();
     startTimer();
   }, 1000);
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  MINUTES.textContent = pad(mins);
-  SECS.textContent = pad(secs);
-});
 
 // Settings
 const SETTINGS = document.querySelector("#settings-button");
@@ -91,14 +109,42 @@ const POMODORO_SLIDER = document.querySelector(".pomodoro-slider");
 const BREAK_SLIDER = document.querySelector(".break-slider");
 const LONG_BREAK_SLIDER = document.querySelector(".long-break-slider");
 
-POMODORO_SLIDER.setAttribute("value", mins);
-BREAK_SLIDER.setAttribute("value", shortBreakTime);
-LONG_BREAK_SLIDER.setAttribute("value", longBreakTime);
+const SLIDERS = document.querySelectorAll(".slider");
+SLIDERS.forEach((slider) => {
+  slider.addEventListener("input", (e) => {
+    let value = e.target.value;
+    if (e.target.classList[0] == "pomodoro-slider") {
+      POMODORO_DURATION.textContent = `${value} minutes`;
+      pomodoro = value;
+      return;
+    }
+    if (e.target.classList[0] == "break-slider") {
+      BREAK_DURATION.textContent = `${value} minutes`;
+      shortBreakTime = value;
+      return;
+    }
+    if (e.target.classList[0] == "long-break-slider") {
+      LONG_BREAK_DURATION.textContent = `${value} minutes`;
+      longBreakTime = value;
+      return;
+    }
+  });
+});
+function init() {
+  // Timer
+  MINUTES.textContent = pad(mins);
+  SECS.textContent = pad(secs);
 
-POMODORO_DURATION.textContent = `${POMODORO_SLIDER.getAttribute(
-  "value"
-)} minutes`;
-BREAK_DURATION.textContent = `${BREAK_SLIDER.getAttribute("value")} minutes`;
-LONG_BREAK_DURATION.textContent = `${LONG_BREAK_SLIDER.getAttribute(
-  "value"
-)} minutes`;
+  // Settings
+  POMODORO_SLIDER.setAttribute("value", mins);
+  BREAK_SLIDER.setAttribute("value", shortBreakTime);
+  LONG_BREAK_SLIDER.setAttribute("value", longBreakTime);
+  POMODORO_DURATION.textContent = `${POMODORO_SLIDER.getAttribute(
+    "value"
+  )} minutes`;
+  BREAK_DURATION.textContent = `${BREAK_SLIDER.getAttribute("value")} minutes`;
+  LONG_BREAK_DURATION.textContent = `${LONG_BREAK_SLIDER.getAttribute(
+    "value"
+  )} minutes`;
+}
+document.addEventListener("DOMContentLoaded", init);
